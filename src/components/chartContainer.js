7d0a669a27b288f593b8ad961b2450334dc45801
novykh <ioannis.klironomos@gmail.com>
2021-09-15 22:58:35 +0300

    Moved keyboard style for cursors into helpers and use them on line component only.

--- src/components/chartContainer.js
+++ src/components/chartContainer.js
@@ -1,49 +1,22 @@
 import React, { useLayoutEffect, useRef } from "react"
 import styled from "styled-components"
-import { useChart, useAttributeValue } from "@/components/provider"
-
-const cursorByNavigation = {
-  default: "default",
-  selectVertical: "row-resize",
-  select: "col-resize",
-  highlight: "crosshair",
-}
-
-const activeCursorByNavigation = {
-  ...cursorByNavigation,
-  pan: "grabbing",
-}
+import { useChart } from "@/components/provider"
 
 const Container = styled.div`
   height: auto !important;
   width: 100% !important;
-  cursor: ${props => cursorByNavigation[props.navigation] || cursorByNavigation.default};
-
-  ${props => {
-    const activeCursor = activeCursorByNavigation[props.navigation]
-    if (!activeCursor) return ""
-    return `
-      &:active {
-        cursor: ${activeCursor};
-      }
-    `
-  }}
-  &:active {
-    cursor: ${props => activeCursorByNavigation[props.navigation] || cursorByNavigation.default};
-  }
 `
 
 const ChartContainer = props => {
   const chart = useChart()
   const ref = useRef()
-  const navigation = useAttributeValue("navigation")
 
   useLayoutEffect(() => {
     chart.getUI().mount(ref.current)
     return () => chart.getUI().unmount()
   }, [])
 
-  return <Container data-testid="chartContent" ref={ref} navigation={navigation} {...props} />
+  return <Container data-testid="chartContent" ref={ref} {...props} />
 }
 
 export default ChartContainer
