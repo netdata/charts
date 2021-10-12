f66970455b34bfb3e8a0e19d1d706823b5befefa
apostolidis <john.apostolidi@gmail.com>
2021-10-12 19:02:27 +0300

    Changed the chart type according to the groupBy.

--- src/sdk/makeChart.js
+++ src/sdk/makeChart.js
@@ -254,6 +254,19 @@ export default ({
     invalidateClosestRowCache()
   })
 
+  let prevCharType = ""
+  node.onAttributeChange("groupBy", groupBy => {
+    if (groupBy !== "dimension") {
+      prevCharType = node.getAttribute("chartType")
+      return node.updateAttribute("chartType", "line")
+    }
+
+    if (node.getAttribute("chartType") === "line") {
+      node.updateAttribute("chartType", prevCharType)
+    }
+    prevCharType = ""
+  })
+
   const getApplicableNodes = (attributes, options) => {
     if (!node.match(attributes)) return [instance]
 
