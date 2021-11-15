20d55b4a3668565ecf870c98f65475ab12371e39
apostolidis <john.apostolidi@gmail.com>
2021-11-15 11:28:46 +0200

    Exported title as a separated component.

--- src/components/line/header/title.js
+++ src/components/line/header/title.js
@@ -1,11 +1,22 @@
 import React from "react"
 import Flex from "@netdata/netdata-ui/lib/components/templates/flex"
 import { Text } from "@netdata/netdata-ui/lib/components/typography"
-import { useTitle, useAttributeValue, useUnit } from "@/components/provider"
+import { useTitle, useAttributeValue, useUnit, withChartProvider } from "@/components/provider"
 
-const Title = props => {
-  const loaded = useAttributeValue("loaded")
+const Unit = () => {
   const unitSign = useUnit()
+
+  if (!unitSign) return null
+
+  return (
+    <Text strong color="textLite" whiteSpace="nowrap">
+      • [{unitSign}]
+    </Text>
+  )
+}
+
+export const Title = props => {
+  const loaded = useAttributeValue("loaded")
   const title = useTitle()
 
   return (
@@ -20,13 +31,9 @@ const Title = props => {
       <Text strong color="textDescription" truncate>
         {title}
       </Text>
-      {loaded && unitSign && (
-        <Text strong color="textLite" whiteSpace="nowrap">
-          • [{unitSign}]
-        </Text>
-      )}
+      {loaded && <Unit />}
     </Flex>
   )
 }
 
-export default Title
+export default withChartProvider(Title)
