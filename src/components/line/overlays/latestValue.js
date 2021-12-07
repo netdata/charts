e4fc8432c131a1b286ecf6a89409a4fc87cc4d49
burbuli8ra <marilena.ioannou.sg@gmail.com>
2021-12-07 13:37:06 +0200

    fix: hide NaN in latest value overlay

--- src/components/line/overlays/latestValue.js
+++ src/components/line/overlays/latestValue.js
@@ -8,7 +8,7 @@ const LatestValue = ({ dimensionId, ...rest }) => {
 
   return (
     <Text strong whiteSpace="nowrap" {...rest}>
-      {!value ? "No data" : `${value} ${unit}`}
+      {!value || isNaN(value) ? "No data" : `${value} ${unit}`}
     </Text>
   )
 }
