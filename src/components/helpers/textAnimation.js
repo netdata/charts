94bb9d183544b0fd5cb0bcff45f5f6bd1445fdd8
John Tsourtis <itsourtis@gmail.com>
2022-02-24 23:52:04 +0200

    change logo color and text-animation (#57)

--- src/components/helpers/textAnimation.js
+++ src/components/helpers/textAnimation.js
@@ -1,23 +1,12 @@
 import { css, keyframes } from "styled-components"
-import { getColor } from "@netdata/netdata-ui/lib/theme/utils"
 
-const getFrames = (startColor, endColor) => keyframes`
-  0% {
-    color:${startColor};
-  }
-  100% {
-    color:${endColor};
-  }
-  `
+const frames = keyframes`
+  from { opacity: 0.4; }
+  to { opacity: 1; }
+`
 
 const textAnimation = css`
-  animation: ${({ theme }) =>
-      getFrames(getColor("border")({ theme }), getColor("primary")({ theme }))}
-    1000ms linear forwards;
-  animation-delay: 0s;
-  animation-iteration-count: infinite;
-  -webkit-backface-visibility: hidden;
-  visibility: visible;
+  animation: ${frames} 1.6s ease-in infinite;
 `
 
 export default textAnimation
