046e37cca5f3bac0a010a90df2cbabd965145f0f
jjtsou <itsourtis@gmail.com>
2022-02-28 16:24:52 +0200

    Set selectedDimensions attribute to default when updating group by attribute

--- src/sdk/filters/makeControllers.js
+++ src/sdk/filters/makeControllers.js
@@ -23,6 +23,7 @@ export default chart => {
     }
     const attributes = getInitialFilterAttributes(chart)
     chart.updateAttributes(attributes)
+    chart.updateAttribute("selectedDimensions", null)
     chart.fetchAndRender()
   }
 
