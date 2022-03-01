3479dd7b24d27f8851868097108dec57e80d45d9
John Tsourtis <itsourtis@gmail.com>
2022-03-01 12:32:20 +0200

    feat/charts-tooltip-should-state-the-units-of-the-charts (#62)
    * Make reusable Unit component
    
    * Use reusable unit component on charts popover and legend

---
+++ src/components/line/dimensions/units.js
@@ -0,0 +1,22 @@
+import React, { memo } from "react"
+import { TextMicro } from "@netdata/netdata-ui/lib/components/typography"
+import { useUnitSign } from "@/components/provider"
+
+const Value = props => (
+  <TextMicro
+    color="textDescription"
+    whiteSpace="nowrap"
+    truncate
+    data-testid="chartDimensions-units"
+    {...props}
+  />
+)
+
+const Units = ({ visible }) => {
+  const units = useUnitSign()
+
+  if (!visible) return null
+  return <Value>{units}</Value>
+}
+
+export default memo(Units)
--- src/components/line/popover/dimension.js
+++ src/components/line/popover/dimension.js
@@ -3,6 +3,7 @@ import Flex from "@netdata/netdata-ui/lib/components/templates/flex"
 import Color from "@/components/line/dimensions/color"
 import Name from "@/components/line/dimensions/name"
 import Value from "@/components/line/dimensions/value"
+import Units from "@/components/line/dimensions/units"
 import { useVisibleDimensionId } from "@/components/provider"
 
 const Dimension = ({ id, strong }) => {
@@ -18,6 +19,7 @@ const Dimension = ({ id, strong }) => {
       <Color id={id} height="12px" />
       <Flex as={Name} flex id={id} strong={strong} maxLength={80} />
       <Value id={id} strong={strong} visible={visible} />
+      <Units visible={visible} />
     </Flex>
   )
 }
