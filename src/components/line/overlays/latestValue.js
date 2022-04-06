fd1a3ce746018508ec955d00baa217ccba651b7d
burbuli8ra <marilena.ioannou.sg@gmail.com>
2022-04-06 13:13:23 +0300

    chore: update interface of Sparkline chart
    * Remove chart's stroke
    * Remove opacity of chart's background color
    * Update appearence and position  of chart's name
    * Update appearence and position of chart's latest value
    * Remove chart's border and add round its corners

--- src/components/line/overlays/latestValue.js
+++ src/components/line/overlays/latestValue.js
@@ -1,15 +1,37 @@
 import React from "react"
-import { Text } from "@netdata/netdata-ui/lib/components/typography"
+import styled from "styled-components"
+import Flex from "@netdata/netdata-ui/lib/components/templates/flex"
+import { Text, TextMicro } from "@netdata/netdata-ui/lib/components/typography"
 import { useLatestValue, useUnitSign } from "@/components/provider"
+
+const StyledText = styled(Text)`
+  text-shadow: 0 18px 28px rgba(9, 30, 66, 0.15), 0 0 1px rgba(9, 30, 66, 0.31);
+`
+
+const StyledTextMicro = styled(TextMicro)`
+  text-shadow: 0 18px 28px rgba(9, 30, 66, 0.15), 0 0 1px rgba(9, 30, 66, 0.31);
+`
 
 const LatestValue = ({ dimensionId, ...rest }) => {
   const unit = useUnitSign()
   const value = useLatestValue(dimensionId)
 
+  const textProps = {
+    color: "textDescription",
+    whiteSpace: "nowrap"
+  }
+
+  if (!value) return (
+    <StyledText strong {...textProps} {...rest}>
+      No data
+    </StyledText>
+  )
+
   return (
-    <Text strong whiteSpace="nowrap" {...rest}>
-      {!value ? "No data" : `${value} ${unit}`}
-    </Text>
+    <Flex alignItems="baseline" gap={1} {...rest}>
+      <StyledText strong {...textProps}>{value}</StyledText>
+      <StyledTextMicro {...textProps}>{unit}</StyledTextMicro>
+    </Flex>
   )
 }
 
