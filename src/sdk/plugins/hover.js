612718b2097f1cf9f27072064f3c09097b8c606d
John Tsourtis <itsourtis@gmail.com>
2022-05-31 16:41:38 +0300

    Revert "Fix/on hover charts render" (#87)
    * Revert "Fix/on hover charts render (#86)"
    
    This reverts commit 53f54c9ca4bb0e9b6f64345fe32b214be4c449b1.
    
    * v0.3.5

--- src/sdk/plugins/hover.js
+++ src/sdk/plugins/hover.js
@@ -6,10 +6,8 @@ export default sdk => {
       })
     })
     .on("hoverChart", chart => {
-      syncChartsOnHover(chart)
       chart.getApplicableNodes({ syncHover: true }).forEach(node => {
         node.updateAttribute("hovering", true)
-        syncChartsOnHover(node)
       })
     })
     .on("blurChart", chart => {
@@ -17,10 +15,4 @@ export default sdk => {
         node.updateAttributes({ hoverX: null, hovering: false })
       })
     })
-}
-
-const syncChartsOnHover = (node = {}) => {
-  if (node.type === "chart" && node.getAttribute("active")) {
-    node.fetchAndRender()
-  }
 }
