34525f8d632659c0d5e32c12d02375b3dd961882
novykh <ioannis.klironomos@gmail.com>
2022-06-14 14:23:54 +0300

    v0.4.6

--- src/components/line/filterToolbox/filters.js
+++ src/components/line/filterToolbox/filters.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Flex from "@netdata/netdata-ui/lib/components/templates/flex"
-import Separator from "@/components/line/separator"
 import Aggregate from "./aggregate"
 import Dimensions from "./dimensions"
 import GroupBy from "./groupBy"
@@ -8,21 +7,7 @@ import DimensionsAggregation from "./dimensionsAggregation"
 import useFiltersToolbox from "./useFiltersToolbox"
 import Reset from "./reset"
 
-export const Container = ({ children, ...rest }) => {
-  const elements = children.reduce((acc, element, index) => {
-    if (!element) return acc
-
-    acc.push(element)
-    if (index < children.length - 1) acc.push(<Separator key={index} />)
-    return acc
-  }, [])
-
-  return (
-    <Flex gap={1} {...rest}>
-      {elements}
-    </Flex>
-  )
-}
+export const Container = ({ children, ...rest }) => <Flex {...rest}>{children}</Flex>
 
 const FilterToolbox = props => {
   const { aggregate, dimensionAggregation, prefixedDimensions } = useFiltersToolbox()
--- src/components/line/filterToolbox/skeleton.js
+++ src/components/line/filterToolbox/skeleton.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Flex from "@netdata/netdata-ui/lib/components/templates/flex"
-import Separator from "@/components/line/separator"
 import FiltersContainer from "./filtersContainer"
 import Label from "./label"
 
@@ -9,9 +8,7 @@ const Skeleton = () => {
     <FiltersContainer>
       <Flex gap={1}>
         <Label width="90px" background="borderSecondary" secondaryLabel="" label="" />
-        <Separator />
         <Label width="120px" background="borderSecondary" />
-        <Separator />
         <Label width="100px" background="borderSecondary" />
       </Flex>
     </FiltersContainer>
