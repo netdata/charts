d1ae7b9c1e0f00460538982d435a565bb68a2446
jjtsou <itsourtis@gmail.com>
2022-08-03 15:42:13 +0300

    StyledItemContainer

--- src/components/line/filterToolbox/chartLabelValues.js
+++ src/components/line/filterToolbox/chartLabelValues.js
@@ -1,19 +1,30 @@
 import { Checkbox, TextSmall } from "@netdata/netdata-ui"
 import { ItemContainer } from "@netdata/netdata-ui/lib/components/drops/menu/dropdownItem"
 import React, { memo } from "react"
+import styled from "styled-components"
+
+const StyledItemContainer = styled(ItemContainer).attrs({
+  padding: [1, 2],
+})``
 
 const ChartLabelValues = ({ labelValues = [], selectedLabels, label, iconProps, onChange }) => {
   return labelValues.map((value, i) => {
     const isValueChecked = selectedLabels[label]?.includes?.(value)
     return (
-      <ItemContainer gap={1} column alignItems="start" key={`${value}_${i}_chartLabelValue`}>
+      <StyledItemContainer
+        gap={1}
+        column
+        alignItems="start"
+        padding={[1, 2]}
+        key={`${value}_${i}_chartLabelValue`}
+      >
         <Checkbox
           iconProps={iconProps}
           checked={!!isValueChecked}
           onChange={() => onChange({ label, value })}
           label={<TextSmall wordBreak="break-word">{value}</TextSmall>}
         />
-      </ItemContainer>
+      </StyledItemContainer>
     )
   })
 }
