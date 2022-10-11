4d6b2b059234d9a6306d01d0a84a62abfa85d79a
Jacek Kolasa <jacek.kolasa@gmail.com>
2022-10-11 15:09:26 +0200

    in mergeMemoizedNodes, add previously not present nodes

--- src/helpers/mergeMemoizedNodes.js
+++ src/helpers/mergeMemoizedNodes.js
@@ -4,7 +4,17 @@ const mergeMemoizedNodes = (prev, next) => {
   if (!prev) return next
   if (deepEqual(prev, next)) return prev
 
-  return prev
+  const results = [...prev]
+  next.forEach(nextNode => {
+    const index = results.findIndex(({ id }) => id === nextNode.id)
+    if (index !== -1) {
+      results[index] = nextNode
+      return
+    }
+    results.push(nextNode)
+  })
+
+  return results
 }
 
 export default mergeMemoizedNodes
