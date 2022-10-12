014c1459cc244e0b4f60069a43c3eed570888570
John Tsourtis <itsourtis@gmail.com>
2022-10-12 13:13:09 +0300

    bug/chart-label-filtering-wrong-selection-on-click (#142)
    * first split, then see if string is included
    
    * introduce util function

--- src/components/line/filterToolbox/chartLabels.js
+++ src/components/line/filterToolbox/chartLabels.js
@@ -12,6 +12,11 @@ import Reset from "./reset"
 import { Collapsible } from "@netdata/netdata-ui"
 import Icon from "@/components/icon"
 
+export const checkIfValueIsSelected = ({ label, value, selectedLabels }) => {
+  const labelValues = selectedLabels[label]?.split?.("|") ?? []
+  return !!labelValues.includes?.(value)
+}
+
 const Item = ({ item, value: selectedLabels, onItemClick }) => {
   const { label, values } = item
   const sortedValues = useMemo(
@@ -120,9 +125,10 @@ const ChartLabels = ({ labelProps, ...rest }) => {
 
   const onLabelValueClick = ({ label, value }) => {
     const labelsToSet = { ...selectedLabels }
-    const shouldAddLabelValue = !labelsToSet[label]?.includes?.(value)
 
-    if (shouldAddLabelValue) {
+    const isValueSelected = checkIfValueIsSelected({ label, value, selectedLabels: labelsToSet })
+
+    if (!isValueSelected) {
       labelsToSet[label] = labelsToSet[label] ? `${labelsToSet[label]}|${value}` : value
     } else {
       if (labelsToSet[label] === value) delete labelsToSet[label]
