5bcf2fdaef3cdbf13f3ae343d6a9adb0704e9e0f
novykh <ioannis.klironomos@gmail.com>
2022-10-15 12:50:39 +0300

    Fixed chart description.

--- src/components/line/details/description.js
+++ src/components/line/details/description.js
@@ -2,43 +2,25 @@ import React from "react"
 import information from "@netdata/netdata-ui/lib/components/icon/assets/information.svg"
 import { TextSmall } from "@netdata/netdata-ui/lib/components/typography"
 import Icon from "@/components/icon"
-import { useAttributeValue, useChart, useTitle } from "@/components/provider"
+import { useAttributeValue, useTitle } from "@/components/provider"
 import Row from "./row"
 
-const DescriptionText = ({ children }) => {
-  const chart = useChart()
-
-  const click = event => {
-    const { hash = "" } = event.target
-
-    if (!hash.startsWith("#menu")) return
-
-    event.preventDefault()
-    chart.sdk.trigger("goToLink", hash.substr(1))
-  }
-
-  return (
-    <TextSmall
-      color="textDescription"
-      dangerouslySetInnerHTML={{ __html: children }}
-      onClick={click}
-    />
-  )
-}
-
-const Description = () => {
+const ChartDescription = () => {
   const title = useTitle()
-  const description = useAttributeValue("description")
+  const info = useAttributeValue("info")
+  const sectionInfo = useAttributeValue("sectionInfo")
 
   return (
     <Row
       icon={<Icon svg={information} color="key" />}
       title={title}
-      data-testid="cartDetails-information"
+      color="key"
+      data-testid="cartDetails-description"
     >
-      {description && <DescriptionText>{description}</DescriptionText>}
+      <TextSmall color="textDescription" dangerouslySetInnerHTML={{ __html: sectionInfo }} />
+      <TextSmall color="textDescription" dangerouslySetInnerHTML={{ __html: info }} />
     </Row>
   )
 }
 
-export default Description
+export default ChartDescription
