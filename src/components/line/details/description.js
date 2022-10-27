845b04bb9a5427935dde17ffe9ace2dc6b320b0f
novykh <ioannis.klironomos@gmail.com>
2022-10-27 16:31:09 +0300

    Added goToLink trigger in description links.

--- src/components/line/details/description.js
+++ src/components/line/details/description.js
@@ -2,13 +2,24 @@ import React from "react"
 import information from "@netdata/netdata-ui/lib/components/icon/assets/information.svg"
 import { TextSmall } from "@netdata/netdata-ui/lib/components/typography"
 import Icon from "@/components/icon"
-import { useAttributeValue, useTitle } from "@/components/provider"
+import { useChart, useAttributeValue, useTitle } from "@/components/provider"
 import Row from "./row"
 
 const ChartDescription = () => {
   const title = useTitle()
   const info = useAttributeValue("info")
   const sectionInfo = useAttributeValue("sectionInfo")
+
+  const chart = useChart()
+
+  const onClick = event => {
+    const { hash = "" } = event.target
+
+    if (!hash.startsWith("#menu")) return
+
+    event.preventDefault()
+    chart.sdk.trigger("goToLink", chart, hash.substr(1))
+  }
 
   return (
     <Row
@@ -17,8 +28,16 @@ const ChartDescription = () => {
       color="key"
       data-testid="cartDetails-description"
     >
-      <TextSmall color="textDescription" dangerouslySetInnerHTML={{ __html: sectionInfo }} />
-      <TextSmall color="textDescription" dangerouslySetInnerHTML={{ __html: info }} />
+      <TextSmall
+        color="textDescription"
+        dangerouslySetInnerHTML={{ __html: sectionInfo }}
+        onClick={onClick}
+      />
+      <TextSmall
+        color="textDescription"
+        dangerouslySetInnerHTML={{ __html: info }}
+        onClick={onClick}
+      />
     </Row>
   )
 }
