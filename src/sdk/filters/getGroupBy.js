c1e3981c9673bfe94fc55af813166cc98c52b6ba
Shyam Sreevalsan <shyam@netdata.cloud>
2022-11-11 13:45:44 +0200

    Update default group-by for nvme charts

--- src/sdk/filters/getGroupBy.js
+++ src/sdk/filters/getGroupBy.js
@@ -19,6 +19,20 @@ const byChartDefaultGroupBy = {
   "httpcheck.status": "_collect_job",
   "httpcheck.response_length": "_collect_job",
   "httpcheck.response_time": "_collect_job",
+  "nvme.device_estimated_endurance_perc": "device",
+  "nvme.device_available_spare_perc": "device",
+  "nvme.device_composite_temperature": "device",
+  "nvme.device_power_cycles_count": "device",
+  "nvme.device_power_on_time": "device",
+  "nvme.device_unsafe_shutdowns_count": "device",
+  "nvme.device_media_errors_rate": "device",
+  "nvme.device_error_log_entries_rate": "device",
+  "nvme.device_warning_composite_temperature_time": "device",
+  "nvme.device_critical_composite_temperature_time": "device",
+  "nvme.device_thermal_mgmt_temp1_transitions_rate": "device",
+  "nvme.device_thermal_mgmt_temp2_transitions_rate": "device",
+  "nvme.device_thermal_mgmt_temp1_time": "device",
+  "nvme.device_thermal_mgmt_temp2_time": "device",
 }
 
 const getDefaultGroupBy = chart => {
