e9078272ae078515a1bd5b799a18f120b6d9b6ba
Panos Christophides <panos@netdata.cloud>
2023-02-28 13:08:33 +0200

    update dropdown container shadow

--- src/components/line/filterToolbox/dropdownTable.js
+++ src/components/line/filterToolbox/dropdownTable.js
@@ -7,8 +7,7 @@ import Label from "./label"
 
 const Container = styled(Flex)`
   ${({ hideShadow }) =>
-    !hideShadow &&
-    "box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2), 0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12);"}
+    !hideShadow && "box-shadow: 0 18px 28px rgba(9, 30, 66, 0.15), 0 0 1px rgba(9, 30, 66, 0.31);"}
   list-style-type: none;
 `
 export const meta = (row, cell, index) => ({
