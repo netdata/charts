9a74fb9ae9953fcc9a1883797a0f7a71335d2e33
novykh <ioannis.klironomos@gmail.com>
2023-04-05 18:56:41 +0300

    Fix multiple args.

--- src/sdk/makeChart/api/fetchCloudData.js
+++ src/sdk/makeChart/api/fetchCloudData.js
@@ -43,12 +43,14 @@ const getPayload = chart => {
           group_by: restPayload["group_by[0]"] || chart.getAttribute("groupBy"),
           ...(!!groupByLabel && { group_by_label: groupByLabel }),
         },
-        ...(!!restPayload["group_by[1]"] && {
+        !!restPayload["group_by[1]"] && {
           aggregation: restPayload["aggregation[1]"] || "avg",
           group_by: restPayload["group_by[1]"] || "",
-          group_by_label: restPayload["group_by[1]"] || "",
-        }),
-      ],
+          ...(!!restPayload["group_by_label[1]"] && {
+            group_by_label: restPayload["group_by_label[1]"],
+          }),
+        },
+      ].filter(Boolean),
       time: {
         time_group,
         // time_group_options: "",
