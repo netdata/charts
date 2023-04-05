6acd50d7c834fcd1a6f4094625f27accb73269de
novykh <ioannis.klironomos@gmail.com>
2023-04-05 23:05:46 +0300

    Fix agent payload.

--- src/sdk/makeChart/api/fetchAgentData.js
+++ src/sdk/makeChart/api/fetchAgentData.js
@@ -16,7 +16,6 @@ const getPayload = chart => {
   } = chart.getAttributes()
 
   const options = getChartURLOptions(chart)
-  const groupByLabel = chart.getAttribute("groupByLabel").join("|")
   const extraPayload = getChartPayload(chart)
 
   return {
@@ -29,11 +28,13 @@ const getPayload = chart => {
     instances: selectedInstances.join("|") || wildcard,
     dimensions: selectedDimensions.join("|") || wildcard,
     labels: selectedLabels.join("|") || wildcard,
-    "group_by[0]": chart.getAttribute("groupBy").join("|"),
-    "group_by_label[0]": groupByLabel.join("|"),
+    "group_by[0]": (extraPayload["group_by[0]"] || chart.getAttribute("groupBy")).join("|"),
+    "group_by_label[0]": (
+      extraPayload["group_by_label[0]"] || chart.getAttribute("groupByLabel")
+    ).join("|"),
     ...(!!extraPayload["group_by[1]"] && {
       aggregation: extraPayload["aggregation[1]"] || "avg",
-      group_by: extraPayload["group_by[1]"] || [],
+      group_by: extraPayload["group_by[1]"].join("|"),
       group_by_label: (extraPayload["group_by_label[1]"] || []).join("|"),
     }),
     "aggregation[0]": aggregationMethod,
