af9018d1319c967e9223ecba5cd8118c9e6cf043
novykh <ioannis.klironomos@gmail.com>
2023-05-05 01:52:46 +0300

    v3.0.35

--- src/components/line/dimensions/value.js
+++ src/components/line/dimensions/value.js
@@ -1,25 +1,23 @@
-import React from "react"
+import React, { forwardRef } from "react"
 import { TextMicro } from "@netdata/netdata-ui"
 import { useConvertedValue } from "@/components/provider"
 
-export const Value = props => (
-  <TextMicro color="textDescription" data-testid="chartDimensions-value" {...props} />
+export const Value = forwardRef((props, ref) => (
+  <TextMicro color="textDescription" data-testid="chartDimensions-value" {...props} ref={ref} />
+))
+
+const ValueComponent = forwardRef(
+  ({ id, visible, valueKey, period = "latest", objKey, Component = Value, ...rest }, ref) => {
+    const value = useConvertedValue(id, period, { valueKey, objKey })
+
+    if (!visible) return null
+
+    return (
+      <Component {...rest} ref={ref}>
+        {value}
+      </Component>
+    )
+  }
 )
-
-const ValueComponent = ({
-  id,
-  visible,
-  valueKey,
-  period = "latest",
-  objKey,
-  Component = Value,
-  ...rest
-}) => {
-  const value = useConvertedValue(id, period, { valueKey, objKey })
-
-  if (!visible) return null
-
-  return <Component {...rest}>{value}</Component>
-}
 
 export default ValueComponent
