29ea1a946b14f08dc45442f2a059018ecd0ae8bf
novykh <ioannis.klironomos@gmail.com>
2023-09-29 23:33:31 +0300

    v3.5.22

--- src/sdk/makeChart/makeGetClosestRow.js
+++ src/sdk/makeChart/makeGetClosestRow.js
@@ -18,17 +18,22 @@ export default chart => {
 
     let start = 0
     let end = data.length - 1
+    let closest = 0
 
     while (start < end) {
       const mid = Math.floor((start + end) / 2)
 
       if (data[mid][0] === timestamp) return mid
 
+      if (Math.abs(data[mid][0] - timestamp) < Math.abs(data[closest][0] - timestamp)) {
+        closest = mid
+      }
+
       if (data[mid][0] < timestamp) start = mid + 1
       else end = mid - 1
     }
 
-    return start
+    return closest
   }
 
   chart.getClosestRow = timestamp => {
