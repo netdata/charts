bcdea774cbe60219f8123cb1f28714d5c1031574
novykh <ioannis.klironomos@gmail.com>
2024-05-20 10:37:44 +0300

    v5.0.7

--- src/sdk/plugins/unitConversion/getConversionUnits.js
+++ src/sdk/plugins/unitConversion/getConversionUnits.js
@@ -1,5 +1,5 @@
 import conversableUnits, { makeConversableKey } from "@/helpers/units/conversableUnits"
-import convert, { getScales, getUnitConfig, unitsMissing, isAdditive } from "@/helpers/units"
+import convert, { getScales, getUnitConfig, unitsMissing, isScalable } from "@/helpers/units"
 
 const scalable = (units, delta, desiredUnits) => {
   const [scaleKeys, scaleByKey] = getScales(units)
@@ -53,7 +53,7 @@ const conversable = (chart, units, delta, desiredUnits) => {
 }
 
 const getMethod = (chart, units, min, max) => {
-  if (unitsMissing(units) || !isAdditive(units)) return ["original"]
+  if (unitsMissing(units) || !isScalable(units)) return ["original"]
 
   const { desiredUnits } = chart.getAttributes()
 
