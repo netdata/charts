a503dfb45d1e3cfa64f75ee8b2a138034967bd24
novykh <ioannis.klironomos@gmail.com>
2026-02-05 09:14:12 +0200

    Remove gradients to allow pinpointing the transitions.

--- src/chartLibraries/dygraph/overlays/alertTransitions.js
+++ src/chartLibraries/dygraph/overlays/alertTransitions.js
@@ -7,7 +7,6 @@ const fillColorMap = {
 }
 
 const OVERLAY_ALPHA = 0.3
-const GRADIENT_WIDTH = 20
 
 const getArea = (dygraph, startMs, endMs) => {
   const [viewStart, viewEnd] = dygraph.xAxisRange()
@@ -48,10 +47,8 @@ export default (chartUI, id) => {
   ctx.globalAlpha = OVERLAY_ALPHA
 
   sortedTransitions.forEach((transition, index) => {
-    const prevTransition = sortedTransitions[index - 1]
-    const nextTransition = sortedTransitions[index + 1]
-
     const startMs = parseTimestamp(transition.timestamp)
+    const nextTransition = sortedTransitions[index + 1]
     const endMs = nextTransition ? parseTimestamp(nextTransition.timestamp) : viewEnd
 
     const toState = transition.to.toUpperCase()
@@ -65,34 +62,6 @@ export default (chartUI, id) => {
     if (!area) return
 
     const { from, width } = area
-
-    if (prevTransition) {
-      const prevState = prevTransition.to.toUpperCase()
-      const prevColor = fillColorMap[prevState]
-      const gradientWidth = Math.min(GRADIENT_WIDTH, width)
-      const skipGradient = !showCleared && prevState === "CLEAR"
-
-      if (prevColor && gradientWidth > 0 && !skipGradient) {
-        const gradient = ctx.createLinearGradient(from, 0, from + gradientWidth, 0)
-        gradient.addColorStop(0, prevColor)
-        gradient.addColorStop(1, toColor)
-
-        ctx.beginPath()
-        ctx.rect(from, 0, gradientWidth, h)
-        ctx.fillStyle = gradient
-        ctx.fill()
-        ctx.closePath()
-
-        if (width > gradientWidth) {
-          ctx.beginPath()
-          ctx.rect(from + gradientWidth, 0, width - gradientWidth, h)
-          ctx.fillStyle = toColor
-          ctx.fill()
-          ctx.closePath()
-        }
-        return
-      }
-    }
 
     ctx.beginPath()
     ctx.rect(from, 0, width, h)
