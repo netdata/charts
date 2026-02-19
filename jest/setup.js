010dcc79f2398f5f792094dcd761b20f7f36d37d
novykh <ioannis.klironomos@gmail.com>
2026-02-19 09:11:07 +0200

    Upgrade node and packages.

--- jest/setup.js
+++ jest/setup.js
@@ -1,4 +1,10 @@
 import "jest-canvas-mock"
+import React from "react"
+import { TextEncoder, TextDecoder } from "util"
+
+globalThis.React = React
+globalThis.TextEncoder = TextEncoder
+globalThis.TextDecoder = TextDecoder
 
 class ResizeObserver {
   observe() {}
