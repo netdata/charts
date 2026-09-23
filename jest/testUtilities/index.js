b232ca25304fe749be73e44f6d0f57ca541077b3
novykh <ioannis.klironomos@gmail.com>
2026-09-23 11:50:36 +0300

    Keep the chart title stable when the page is translated

--- jest/testUtilities/index.js
+++ jest/testUtilities/index.js
@@ -1,3 +1,4 @@
 export { renderWithChart, renderHookWithChart, renderWithProviders } from "./renderUtils"
 export { makeTestChart } from "./makeTestChart"
 export { loadHeatmapPayload, makeHeatmapPayload } from "./heatmapPayload"
+export { simulateTranslate, collectRenderErrors } from "./translation"
